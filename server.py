203f562c24cc70beed9e4fe331665ef8c886b6ca
Yaolong Huang <airekans@gmail.com>
2014-04-17 21:20:31 +0800

    Remove useless code in server.

--- server.py
+++ server.py
@@ -1,7 +1,4 @@
-import gevent.server
-import struct
 from proto import simple_pb2
-import protocodec
 import rpc
 from gevent import monkey
 monkey.patch_all()
@@ -9,41 +6,7 @@ monkey.patch_all()
 import db
 
 
-class ProtocolServer(object):
-
-    def __init__(self):
-        self.__req_handlers = {}
-
-    def register_handler(self, req_cls, handler):
-        # TODO: double check the performance of hashing long string
-        self.__req_handlers[req_cls.DESCRIPTOR.full_name] = handler
-
-    def handle_req(self, req):
-        req_full_name = req.DESCRIPTOR.full_name
-        try:
-            handler = self.__req_handlers[req_full_name]
-        except KeyError as e:
-            print 'Cannot find handler for %s: %s' % (req_full_name, str(e))
-            return ProtocolServer.get_error_rsp(1, 'cannot find handler for ' + req_full_name)
-
-        try:
-            rsp = handler(req)
-        except:
-            import traceback
-            traceback.print_exc()
-            return ProtocolServer.get_error_rsp(2, 'Exception when handling ' + req_full_name)
-
-        return rsp
-
-    @staticmethod
-    def get_error_rsp(return_code, err_msg):
-        rsp = simple_pb2.SimpleResponse()
-        rsp.return_code = return_code
-        rsp.msg = err_msg
-        return rsp
-
-
-class MonsitServer(object):
+class MonsitServiceImpl(simple_pb2.MonsitService):
 
     def __init__(self):
         with db.DBConnection() as cnx:
@@ -53,120 +16,37 @@ class MonsitServer(object):
 
             self.__registered_hosts = registered_hosts
 
-    def handle_register_req(self, req):
-        # if the host is not in DB yet, insert it into db
+    def Register(self, rpc_controller, request, done):
         try:
-            host_info = self.__registered_hosts[req.host_name]
+            host_info = self.__registered_hosts[request.host_name]
             host_info[1] = True
         except KeyError:
             with db.DBConnection() as cnx:
-                host_info = cnx.insert_new_host(req.host_name)
-                self.__registered_hosts[req.host_name] = [host_info[0], True]
+                host_info = cnx.insert_new_host(request.host_name)
+                self.__registered_hosts[request.host_name] = [host_info[0], True]
 
-        return ProtocolServer.get_error_rsp(0, '')
-
-    def handle_simple_req(self, req):
-        if req.host_name not in self.__registered_hosts or \
-           not self.__registered_hosts[req.host_name][1]:
-            print 'Host not registered:', req.host_name
-            return ProtocolServer.get_error_rsp(3, 'Not registered')
-
-        print req
-
-        if len(req.net_infos) > 0:
-            with db.DBConnection() as cnx:
-                host_id = self.__registered_hosts[req.host_name][0]
-                cnx.create_host_tables(host_id)
-                if not cnx.insert_host_info(req, host_id):
-                    print 'failed to store req in db'
-
-        rsp = simple_pb2.SimpleResponse()
-        rsp.return_code = 0
-        rsp.msg = 'success'
-        return rsp
-
-
-class MonsitServiceImpl(simple_pb2.MonsitService):
-    def Register(self, rpc_controller, request, done):
-        print 'Register got called'
-        rsp = simple_pb2.RegisterResponse()
-        rsp.return_code = 0
-        rsp.msg = 'SUCCESS'
+        rsp = simple_pb2.RegisterResponse(return_code=0, msg='SUCCESS')
         return rsp
 
     def Report(self, rpc_controller, request, done):
-        print 'Report got called'
-        rsp = simple_pb2.SimpleResponse()
-        rsp.return_code = 0
-        rsp.msg = 'SUCCESS'
+        if request.host_name not in self.__registered_hosts or \
+           not self.__registered_hosts[request.host_name][1]:
+            print 'Host not registered:', request.host_name
+            rsp = simple_pb2.SimpleResponse(return_code=1, msg='Host not registered')
+            return rsp
+
+        print request
+
+        if len(request.net_infos) > 0:
+            with db.DBConnection() as cnx:
+                host_id = self.__registered_hosts[request.host_name][0]
+                cnx.create_host_tables(host_id)
+                if not cnx.insert_host_info(request, host_id):
+                    print 'failed to store req in db'
+
+        rsp = simple_pb2.SimpleResponse(return_code=0, msg='SUCCESS')
         return rsp
 
-
-def print_binary_string(bin_str):
-    for c in bin_str:
-        print ord(c),
-    print ''
-
-
-_pb_server = ProtocolServer()
-_monsit_server = None
-
-
-def init_pb_server():
-    global _pb_server, _monsit_server
-
-    _monsit_server = MonsitServer()
-
-    _pb_server.register_handler(simple_pb2.SimpleRequest,
-                                _monsit_server.handle_simple_req)
-    _pb_server.register_handler(simple_pb2.RegisterRequest,
-                                _monsit_server.handle_register_req)
-
-
-def handle(socket, addr):
-    print addr
-
-    content = ""
-    while True:
-        try:
-            recv_buf = socket.recv(1024)
-            if len(recv_buf) == 0:
-                break
-        except Exception, e:
-            print e
-            break
-
-        content += recv_buf
-        mem_content = memoryview(content)
-        cur_index = 0
-        while cur_index < len(content):
-            if len(mem_content[cur_index:]) < 6:
-                break
-            elif mem_content[cur_index:cur_index + 2] != 'PB':
-                cur_index += 2  # skip the first 2 bytes
-                break
-
-            (buf_size,) = struct.unpack('!I',
-                                        mem_content[cur_index + 2: cur_index + 6].tobytes())
-            if len(mem_content[cur_index + 6:]) < buf_size:
-                break
-
-            pb_buf = mem_content[cur_index + 6: cur_index + 6 + buf_size].tobytes()
-            cur_index += buf_size + 6
-            result = protocodec.parse_message(pb_buf)
-            if result is None:
-                print 'pb decode error, skip this message'
-                break
-            _, req = result
-
-            rsp = _pb_server.handle_req(req)
-            serialized_rsp = protocodec.serialize_message(rsp)
-            socket.send(serialized_rsp)
-
-        if cur_index > 0:
-            content = content[cur_index:]
-
-    print addr, 'has disconnected'
 
 if __name__ == '__main__':
     db.init()
