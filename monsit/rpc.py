401941840b0ac9067a9190d52c70c56de87f315b
Yaolong Huang <airekans@gmail.com>
2014-04-24 23:22:34 +0800

    Change the async support to use 2 greenlets.
    I use 2 greenlets to read and write to the socket separately, so it will be fully asynchronous.

--- monsit/rpc.py
+++ monsit/rpc.py
@@ -78,8 +78,13 @@ class TcpChannel(google.protobuf.service.RpcChannel):
         self._socket = gevent.socket.socket()
         self.connect()
 
-        self._req_queue = gevent.queue.Queue()
-        gevent.spawn(self.worker_run)
+        self._send_task_queue = gevent.queue.Queue()
+        self._recv_infos = {}
+        self._workers = [gevent.spawn(self.send_loop), gevent.spawn(self.recv_loop)]
+
+    def __del__(self):
+        self._socket.close()
+        gevent.killall(self._workers)
 
     def _get_flow_id(self):
         flow_id = self._flow_id
@@ -104,10 +109,71 @@ class TcpChannel(google.protobuf.service.RpcChannel):
 
         return recv_buf
 
-    def worker_run(self):
+    def recv_loop(self):
+        expected_size = 2 + struct.calcsize("!I")
         while True:
-            work_fun = self._req_queue.get()
-            work_fun()
+            pb_buf = self._recv(expected_size)
+            if len(pb_buf) == 0:
+                print 'socket has been closed'
+                break
+            if len(pb_buf) < expected_size:
+                print 'rsp buffer broken'
+                continue
+            if pb_buf[:2] != 'PB':
+                print 'rsp buffer not begin with PB'
+                continue
+
+            buf_size = struct.unpack("!I", pb_buf[2:])[0]
+            pb_buf = self._recv(buf_size)
+            if len(pb_buf) == 0:
+                print 'socket has been closed'
+                break
+
+            result = parse_meta(pb_buf)
+            if result is None:
+                print 'pb decode error, skip this message'
+                continue
+
+            meta_len, pb_msg_len, meta_info = result
+
+            if meta_info.flow_id in self._recv_infos:
+                expected_meta, rpc_controller, response_class, done = \
+                    self._recv_infos[meta_info.flow_id]
+
+                if meta_info.flow_id != expected_meta.flow_id:
+                    rpc_controller.SetFailed('rsp flow id not match')
+                    print 'rsp flow id not match:', expected_meta.flow_id, meta_info.flow_id
+                    done(None)
+                    continue
+                elif meta_info.service_name != expected_meta.service_name or \
+                                meta_info.method_name != expected_meta.method_name or \
+                                meta_info.msg_name != response_class.DESCRIPTOR.full_name:
+                    rpc_controller.SetFailed('rsp meta not match')
+                    print 'rsp meta not match'
+                    done(None)
+                    continue
+
+                rsp = _parse_message(pb_buf[8 + meta_len:8 + meta_len + pb_msg_len],
+                                     response_class)
+                done(rsp)
+            else:
+                print 'flow id not found:', meta_info.flow_id
+
+    def send_loop(self):
+        while True:
+            send_task = self._send_task_queue.get()
+            send_task()
+
+    def send_req(self, method_descriptor, rpc_controller, request, response_class, done):
+        service_descriptor = method_descriptor.containing_service
+        flow_id = self._get_flow_id()
+        meta_info = rpc_meta_pb2.MetaInfo()
+        meta_info.flow_id = flow_id
+        meta_info.service_name = service_descriptor.full_name
+        meta_info.method_name = method_descriptor.name
+        serialized_req = _serialize_message(meta_info, request)
+        self._socket.send(serialized_req)
+        self._recv_infos[flow_id] = meta_info, rpc_controller, response_class, done
 
     def call_method(self, method_descriptor, rpc_controller, request, response_class, done):
         service_descriptor = method_descriptor.containing_service
@@ -123,10 +189,12 @@ class TcpChannel(google.protobuf.service.RpcChannel):
         if len(pb_buf) < expected_size:
             rpc_controller.SetFailed('rsp buffer broken')
             print 'rsp buffer broken'
+            done(None)
             return None
         if pb_buf[:2] != 'PB':
             rpc_controller.SetFailed('rsp buffer not begin with PB')
             print 'rsp buffer not begin with PB'
+            done(None)
             return None
         buf_size = struct.unpack("!I", pb_buf[2:])[0]
         pb_buf = self._recv(buf_size)
@@ -134,22 +202,26 @@ class TcpChannel(google.protobuf.service.RpcChannel):
         if result is None:
             rpc_controller.SetFailed('pb decode meta error')
             print 'pb decode error, skip this message'
+            done(None)
             return None
         meta_len, pb_msg_len, meta_info = result
         if meta_info.flow_id != flow_id:
             rpc_controller.SetFailed('rsp flow id not match')
             print 'rsp flow id not match:', flow_id, meta_info.flow_id
+            done(None)
             return None
         elif meta_info.service_name != service_descriptor.full_name or \
                         meta_info.method_name != method_descriptor.name or \
                         meta_info.msg_name != response_class.DESCRIPTOR.full_name:
             rpc_controller.SetFailed('rsp meta not match')
             print 'rsp meta not match'
+            done(None)
             return None
         rsp = _parse_message(pb_buf[8 + meta_len:8 + meta_len + pb_msg_len],
                              response_class)
         if rsp is None:
             rpc_controller.SetFailed('pb decode rsp error')
+            done(rsp)
             return None
         if done is not None:
             done(rsp)
@@ -164,12 +236,12 @@ class TcpChannel(google.protobuf.service.RpcChannel):
         if done is None:
             res = gevent.event.AsyncResult()
             done = lambda rsp: res.set(rsp)
-            self._req_queue.put_nowait(lambda: self.call_method(method_descriptor, rpc_controller,
-                                                                request, response_class, done))
+            self._send_task_queue.put_nowait(lambda: self.send_req(method_descriptor, rpc_controller,
+                                                                   request, response_class, done))
             return res.get()
         else:
-            self._req_queue.put_nowait(lambda: self.call_method(method_descriptor, rpc_controller,
-                                                                request, response_class, done))
+            self._send_task_queue.put_nowait(lambda: self.send_req(method_descriptor, rpc_controller,
+                                                                   request, response_class, done))
             return None
 
 
